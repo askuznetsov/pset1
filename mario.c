e8a33649594ce76a605c05127c571e8872b28903
askuznetsov <askuznetsov6996@gmail.com>
2017-02-12 18:05:26 +0200

    Update mario.c

--- mario.c
+++ mario.c
@@ -12,17 +12,22 @@ int main (void)
         printf("Height: ");
         height = get_int();
     } while (height < 0 || height > 23);
+    //цыкл пирамиды слева
     for(all_rows = 1; all_rows <= height; all_rows++)
     {
+        //делаем пробелы
         for(space = 0; space < height - all_rows; space++)
         {
             printf(" ");
         }
+        //и рисуем '#'
         for(hash = 0; hash < all_rows; hash++)
         {
             printf("#");
         }
+        //создаем поле между полу - пирамидами
         printf("  ");
+        //цыкл пирамиды направо
         for(int i = 0; i < all_rows; i++)
         {
            printf("#");
